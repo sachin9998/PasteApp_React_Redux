6694511ebb85f2acc51d908e8cda08804c69c292
Sachin Alam <sachinalam4216@gmail.com>
2024-09-30 20:05:59 +0530

    stage 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import { useSelector } from "react-redux";
+import { createBrowserRouter } from "react-router-dom";
 import "./App.css";
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: "<div></div>",
+  },
+  {
+    path: "/pastes",
+    element: "<div></div>",
+  },
+  {
+    path: "/pastes/:id",
+    element: "<div></div>",
+  },
+]);
 
-  
+function App() {
+  // <Routes>
+  //   <Route path="/" element={<Home />} />
+  //   <Route path="/about" element={<About />} />
+  //   <Route path="/users" element={<Users />} />
+  // </Routes>;
 
   return (
     <>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import store from "./store.js";
@@ -8,7 +9,9 @@ import store from "./store.js";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
